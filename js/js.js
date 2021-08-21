947883fcebf1158f35db37dad3c8cd645155ffb3
aboodKahlous <86069394+aboodKahlous@users.noreply.github.com>
2021-08-21 18:40:20 +0300

    desgin web site for animy

---
+++ js/js.js
@@ -0,0 +1,23 @@
+var typed = new Typed('.type', {
+    strings: [
+
+        '',
+        'Did You Search For An Anime ??',
+        'Now You Can Whath anime any time',
+        'Lets Sign in For Start',
+        'Have a great day!'
+        
+        ],
+
+        typeSpeed: 60   ,
+        startDelay: 300,
+        backSpeed: 60,
+        smartBackspace: true,
+        backDelay: 700,
+        fadeOut: true,
+        fadeOutClass: 'typed-fade-out',
+        fadeOutDelay: 500,
+        loop: true,
+        loopCount: 3,
+
+  });
